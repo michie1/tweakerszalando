0046a7d921435e141a4d43722e0a09f500ff26fb
Michiel <michiel@blue-donut.com>
2017-05-12 20:20:43 +0200

    Zalando has its own topic

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ pref_link = ''
 
 def do(sc):
     global pref_link
-    url = 'https://gathering.tweakers.net/rss/list_messages/1543297'
+    url = 'https://gathering.tweakers.net/rss/list_messages/1764859'
     data = urllib2.urlopen(url)
     first = xml.etree.ElementTree.parse(data).getroot()[0][10]
     description = first[3].text
@@ -17,13 +17,13 @@ def do(sc):
 
     #if 'helaas' in first_description or 
     if link != pref_link:
-        if 'zalando' in description or 'Zalando' in description:
+        # if 'zalando' in description or 'Zalando' in description: # Niet nodig meer, want apart Zalando topic
             #pynotify.init('Zalando')
             #notice = pynotify.Notification('Tweakers.net', 'Zalando kortingsbon')
             #notice.show()
-            print(link, pref_link)
-            subprocess.Popen(['notify-send', 'Tweakers.net', 'Zalando kortingsbon'])
-            pref_link = link
+        print(link, pref_link)
+        subprocess.Popen(['notify-send', 'Tweakers.net', 'Zalando kortingsbon'])
+        pref_link = link
 
     sc.enter(60, 1, do, (sc,))
 
